a20f119d0085835ad1821c5f02b454333b82ba29
Kevin Jiang <kjiang@imac-2.local>
2019-01-26 21:57:47 -0500

    stringIndex change

--- aUI/Concentration/ConcentrationViewController.swift
+++ aUI/Concentration/ConcentrationViewController.swift
@@ -58,12 +58,15 @@ class ConcentrationViewController: UIViewController {
     }
     
     
-    private var emojiChoices = ["👻", "🎃", "🙇🏻‍♂️", "💁🏻‍♀️","💩","💀","👽","🤖","☃️"]
+    //private var emojiChoices = ["👻", "🎃", "🙇🏻‍♂️", "💁🏻‍♀️","💩","💀","👽","🤖","☃️"]
+        //emoji[card] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+    private var emojiChoices = "👻🎃🙇🏻‍♂️💁🏻‍♀️💩💀👽🤖☃️"
     private var emoji = [Card:String]()
     
     private func emoji(for card: Card) -> String {
         if emoji[card] == nil,  emojiChoices.count > 0 {
-            emoji[card] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+            let randomStringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: emojiChoices.count.arc4random)
+            emoji[card] = String(emojiChoices.remove(at: randomStringIndex))
         }
         return emoji[card] ?? "?"
     }
