16581a6f4c8833e8403afb1764151c8465dcbb2f
Kevin Jiang <kjiang@imac-2.local>
2019-04-05 23:38:39 -0400

    checkbox

---
+++ aUI/FrameworkViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FrameworkViewController.swift
+//  aUI
+//
+//  Created by Haojun Jiang on 4/5/19.
+//  Copyright © 2019 Haojun Jiang. All rights reserved.
+//
+
+import UIKit
+import Checkbox
+
+class FrameworkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBOutlet weak var checkbox: Checkbox!
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
