234f19b34dacabc1b1384aceba64f1471036a7c9
Kevin Jiang <kjiang@imac-2.local>
2019-04-14 11:29:39 -0400

    Gesture Pan

--- aUI/FrameworkViewController.swift
+++ aUI/FrameworkViewController.swift
@@ -19,14 +19,16 @@ class FrameworkViewController: UIViewController {
     
 
     @IBOutlet weak var checkbox: Checkbox!
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func handlePan(_ recognizer: UIPanGestureRecognizer) {
+        guard let recognizerView = recognizer.view else {
+            return
+        }
+        
+        let translation = recognizer.translation(in: view)
+        recognizerView.center.x += translation.x
+        recognizerView.center.y += translation.y
+        recognizer.setTranslation(.zero, in: view)
     }
-    */
 
 }
