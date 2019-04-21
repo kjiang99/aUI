d6506c73df046f1a5763c6e73f9f2b4a4d5a4713
Haojun Jiang <kjiang22206@yahoo.com>
2019-04-20 20:48:28 -0400

    PackingList animation

---
+++ aUI/DrawSample/PackingList/PackingListViewController.swift
@@ -0,0 +1,54 @@
+//
+//  PackingListViewController.swift
+//  aUI
+//
+//  Created by Haojun Jiang on 4/19/19.
+//  Copyright © 2019 Haojun Jiang. All rights reserved.
+//
+
+import UIKit
+
+class PackingListViewController: UIViewController {
+    
+    var menuIsOpen = false
+    var slider: HorizontalItemList!
+    var items: [Int] = [5, 6, 7]
+    
+    @IBOutlet var titleLabel: UILabel!
+    
+    @IBOutlet var menuHeightConstraint: NSLayoutConstraint!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        makeSlider()
+    }
+
+    
+    @IBAction func toggleMenu(_ sender: AnyObject) {
+        menuIsOpen = !menuIsOpen
+        
+        titleLabel.text = menuIsOpen ? "Select Item!!!!!!!!!!!!!" : "Packing List"
+        menuHeightConstraint.constant = menuIsOpen ? 200 : 80
+        
+        UIView.animate(
+            withDuration: 0.67,
+            delay: 0.0,
+            options: .curveEaseIn,
+            animations: {
+                self.view.layoutIfNeeded()
+        },
+            completion: nil
+        )
+    }
+    
+    func makeSlider() {
+        slider = HorizontalItemList(inView: view)
+        slider.didSelectItem = {index in
+            self.items.append(index)
+//            self.tableView.reloadData()
+//            self.transitionCloseMenu()
+        }
+        self.titleLabel.superview?.addSubview(slider)
+    }
+    
+}
