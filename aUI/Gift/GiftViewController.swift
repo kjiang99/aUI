f4096bc348447df56a392fd1883ff388025e5f68
Haojun Jiang <kjiang22206@yahoo.com>
2019-10-03 22:39:34 -0400

    gift card horizontal collection view

--- aUI/Gift/GiftViewController.swift
+++ aUI/Gift/GiftViewController.swift
@@ -10,22 +10,33 @@ import UIKit
 
 class GiftViewController: UIViewController {
     
-    @IBOutlet weak var GiftCardCollectionView: UICollectionView!
+    @IBOutlet weak var giftCardCollectionView: UICollectionView!
+    @IBOutlet weak var giftCardCollectionViewHeightConstraint: NSLayoutConstraint!
     
     var giftCards: [GiftCardModel] = []
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        GiftCardCollectionView.dataSource = self
-        GiftCardCollectionView.delegate = self
+        giftCardCollectionView.dataSource = self
+        giftCardCollectionView.delegate = self
         
         GiftCardFunctions.getGiftCards {[weak self] (cards) in
             guard let self = self else { return }
             self.giftCards = cards
-            self.GiftCardCollectionView.reloadData()
+            self.giftCardCollectionView.reloadData()
         }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setHeightOfCollectionView()
+    }
+    
+    private func setHeightOfCollectionView(){
+        let width = giftCardCollectionView.bounds.width - 30
+        let height = width / 1.5
+        giftCardCollectionViewHeightConstraint.constant = height
     }
 }
 
@@ -43,13 +54,15 @@ extension GiftViewController: UICollectionViewDataSource, UICollectionViewDelega
     
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let columns: CGFloat = 2
-        let collectionViewWidth = collectionView.bounds.width
-        let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
-        let spaceBetweenCells = flowLayout.minimumInteritemSpacing * (columns - 1)
-        let adjustedWidth = collectionViewWidth - spaceBetweenCells
-        let width: CGFloat = floor(adjustedWidth / columns)
-        let height: CGFloat = width / 1.5
+        let width = giftCardCollectionView.bounds.width - 50
+        let height = width / 1.5
+//        let columns: CGFloat = 2
+//        let collectionViewWidth = collectionView.bounds.width
+//        let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
+//        let spaceBetweenCells = flowLayout.minimumInteritemSpacing * (columns - 1)
+//        let adjustedWidth = collectionViewWidth - spaceBetweenCells
+//        let width: CGFloat = floor(adjustedWidth / columns)
+//        let height: CGFloat = width / 1.5
         return CGSize(width: width, height: height)
     }
 }
