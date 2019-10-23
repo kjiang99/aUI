2cbf05bfb9e130f6d7eaf3684804538c7ded3787
Haojun Jiang <haojun.jiang@gdit.com>
2019-10-23 17:05:57 -0400

    change zip masked character to black dot;  Change dice dynamic add controls

--- aUI/DiceViewController.swift
+++ aUI/DiceViewController.swift
@@ -9,74 +9,80 @@
 import UIKit
 
 class DiceViewController: UIViewController {
-
-    @IBOutlet weak var firstDice: UIImageView!
-    @IBOutlet weak var secondDice: UIImageView!
-    
-    var firstValue: Int?
-    var secondValue: Int?
-    
-    
-    override func viewWillAppear(_ animated: Bool) {
-        if let firstValue = self.firstValue {
-            self.firstDice.image = UIImage(named: "d\(firstValue)")
-        } else {
-            self.firstDice.image = nil
-        }
-        
-        if let secondValue = self.secondValue {
-            self.secondDice.image = UIImage (named: "d\(secondValue)")
-        } else {
-            self.secondDice.image = nil
-        }
-        
-        self.firstDice.alpha = 0
-        self.secondDice.alpha = 0
+  
+  @IBOutlet weak var firstDice: UIImageView!
+  @IBOutlet weak var secondDice: UIImageView!
+  
+  var firstValue: Int?
+  var secondValue: Int?
+  
+  
+  override func viewWillAppear(_ animated: Bool) {
+    if let firstValue = self.firstValue {
+      self.firstDice.image = UIImage(named: "d\(firstValue)")
+    } else {
+      self.firstDice.image = nil
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        UIView.animate(withDuration: 2) {
-            self.firstDice.alpha = 1
-            self.secondDice.alpha = 1
-        }
+    if let secondValue = self.secondValue {
+      self.secondDice.image = UIImage (named: "d\(secondValue)")
+    } else {
+      self.secondDice.image = nil
     }
     
-    var count  = 0
-    var label: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let label = UILabel()
-        label.frame = CGRect.init(x: 120, y: 503, width: 150, height: 150)
-        label.text = "0"
-        self.view.addSubview(label)
-        self.label = label
-        
-        let button = UIButton()
-        button.frame = CGRect.init(x: 120, y: 580, width: 80, height: 60)
-        button.setTitle("codeClick", for: .normal)
-        button.setTitleColor(UIColor.brown, for: .normal)
-        self.view.addSubview(button)
-        button.addTarget(self, action: #selector(self.incrementCount), for: UIControlEvents.touchUpInside)
+    self.firstDice.alpha = 0
+    self.secondDice.alpha = 0
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    UIView.animate(withDuration: 2) {
+      self.firstDice.alpha = 1
+      self.secondDice.alpha = 1
     }
-
+  }
+  
+  var count  = 0
+  let codeLabel: UILabel = {
+    let codeLabel = UILabel()
+    //codeLabel.translatesAutoresizingMaskIntoConstraints = false
+    codeLabel.text = "0"
+    codeLabel.sizeToFit()
+    codeLabel.frame = CGRect.init(x: 120, y: 503, width: 150, height: 150)
+    return codeLabel
+  }()
+  let codeButton: UIButton = {
+    let codeButton = UIButton()
+    //codeButton.translatesAutoresizingMaskIntoConstraints = false
+    codeButton.setTitle("codeClick", for: .normal)
+    codeButton.setTitleColor(.brown, for: .normal)
+    codeButton.frame = CGRect.init(x: 120, y: 580, width: 80, height: 60)
+    codeButton.addTarget(self, action: #selector(codeIncrementCount), for: UIControlEvents.touchUpInside)
+    return codeButton
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    @IBOutlet weak var uiLabel: UILabel!
-    
-    @IBAction func uiIncreamentCount () {
-        self.count = self.count + 1
-        self.uiLabel.text = "\(self.count)"
-    }
-    
-    
-    @objc func incrementCount () {
-        self.count = self.count + 1
-        self.label.text = "\(self.count)"
-    }
-    
-    
-    @IBAction func dismiss () {
-        self.dismiss(animated: true, completion: nil)
-    }
+    self.view.addSubview(codeLabel)
+    self.view.addSubview(codeButton)
+  }
+  
+  
+  @IBOutlet weak var uiLabel: UILabel!
+  
+  @IBAction func uiIncreamentCount () {
+    self.count = self.count + 1
+    self.uiLabel.text = "\(self.count)"
+  }
+  
+  
+  @objc func codeIncrementCount () {
+    self.count = self.count + 1
+    self.codeLabel.text = "\(self.count)"
+  }
+  
+  
+  @IBAction func dismiss () {
+    self.dismiss(animated: true, completion: nil)
+  }
 }
--- aUI/Text Field/ZipCodeTextFieldDelegate.swift
+++ aUI/Text Field/ZipCodeTextFieldDelegate.swift
@@ -10,20 +10,34 @@ import Foundation
 import UIKit
 
 class ZipCodeTextFieldDelegate: NSObject, UITextFieldDelegate {
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {        
-        var newText = textField.text! as NSString
-        newText = newText.replacingCharacters(in: range, with: string) as NSString
-        
-        return newText.length <= 5
+  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    var newText = textField.text! as NSString
+    newText = newText.replacingCharacters(in: range, with: string) as NSString
+    
+    return newText.length <= 5
+  }
+  
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    if let text = textField.text {
+      textField.text = text.maskStringWithDotsAtEnd(i: 3)
+    }
+  }
+}
+
+extension String {
+  func maskStringWithDotsAtEnd (i: Int) -> String {
+    var maskedString = ""
+    let passwordDot = "\u{000025CF}" //●
+    
+    if i > 0 {
+      if self.count > i {
+        maskedString = String(self.prefix(self.count - i)) + String(repeating: passwordDot, count: i)
+      }
+      else {
+        maskedString = String(repeating: passwordDot, count: self.count)
+      }
     }
     
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        if let text = textField.text {
-            if text.count > 3 {
-                textField.text = String(text.prefix(text.count - 3)) + "***"
-            } else {
-                textField.text = String(repeating: "*", count: text.count)
-            }
-        }
-    }
+    return maskedString
+  }
 }
