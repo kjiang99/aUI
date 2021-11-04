6241c44827c42b8c9f3a2b42e84013483e4878af
Haojun Jiang <haojun.jiang@gdit.com>
2021-11-04 17:03:49 -0400

    XCode13; Start to dev Custom Slider

---
+++ aUI/CustomSlider/CustomSliderViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CustomSliderViewController.swift
+//  aUI
+//
+//  Created by Haojun Jiang on 11/4/21.
+//  Copyright © 2021 Haojun Jiang. All rights reserved.
+//
+
+import UIKit
+import Checkbox
+
+class CustomSliderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Custom Slider"
+    }
+}
